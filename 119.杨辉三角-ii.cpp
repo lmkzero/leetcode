331d6caa2ff0e3b9ac0eacc8d66f293dda9ade87
mengkailiu111 <dave_lmk@outlook.com>
2021-09-22 23:32:39 +0800

    119.杨辉三角II

---
+++ 119.杨辉三角-ii.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=cpp
+ *
+ * [119] 杨辉三角 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> nums(rowIndex + 1);
+        nums[0] = 1;
+        for (int i = 1; i <= rowIndex; ++i) {
+            for (int j = i; j >= 1; --j) {
+                nums[j] += nums[j - 1];
+            }
+        }
+        return nums;
+    }
+};
+// @lc code=end
+
