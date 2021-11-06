8245baf27bd6f037db687ebb3e5f896b3e46bd2d
mengkailiu111 <dave_lmk@outlook.com>
2021-11-06 20:50:52 +0800

    168.excel表列名称

---
+++ 168.excel表列名称.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=168 lang=cpp
+ *
+ * [168] Excel表列名称
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        // 转进制
+        string ans;
+        while (columnNumber > 0) {
+            int a0 = (columnNumber - 1) % 26 + 1;
+            ans += a0 - 1 + 'A';
+            columnNumber = (columnNumber - a0) / 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+// @lc code=end
+
