3b75de1b849085ed8e9ee96b5bef02e06ea9355a
mengkailiu <dave_lmk@outlook.com>
2023-02-22 21:27:01 +0800

    7. 整数反转 --go

---
+++ golang/7_reverse_integer_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+func Test_reverse(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				x: 123,
+			},
+			want: 321,
+		},
+		{
+			name: "2",
+			args: args{
+				x: -123,
+			},
+			want: -321,
+		},
+		{
+			name: "3",
+			args: args{
+				x: 120,
+			},
+			want: 21,
+		},
+		{
+			name: "4",
+			args: args{
+				x: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverse(tt.args.x); got != tt.want {
+				t.Errorf("reverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
