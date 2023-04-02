73b60773e9de423d982ee0b2e3d99184af11c568
mengkailiu <dave_lmk@outlook.com>
2023-04-02 11:51:41 +0800

    29. 两数相除 --go

---
+++ golang/29_divide_two_integers_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func Test_divide(t *testing.T) {
+	type args struct {
+		dividend int
+		divisor  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				dividend: 10,
+				divisor:  3,
+			},
+			want: 3,
+		},
+		{
+			name: "2",
+			args: args{
+				dividend: 7,
+				divisor:  -3,
+			},
+			want: -2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := divide(tt.args.dividend, tt.args.divisor); got != tt.want {
+				t.Errorf("divide() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
