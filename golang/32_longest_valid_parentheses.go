f1374bfcf0a4876e8ed4ce4fa3984b55f9a22ab7
mengkailiu <dave_lmk@outlook.com>
2023-04-09 12:16:11 +0800

    32. 最长有效的括号 --go

---
+++ golang/32_longest_valid_parentheses.go
@@ -0,0 +1,20 @@
+package main
+
+func longestValidParentheses(s string) int {
+	ans := 0
+	stack := make([]int, 0, len(s))
+	stack = append(stack, -1)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			stack = append(stack, i)
+		} else {
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+			} else {
+				ans = max(ans, i-stack[len(stack)-1])
+			}
+		}
+	}
+	return ans
+}
