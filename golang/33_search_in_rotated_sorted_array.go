667e86f683bd02f5e50464c1044257cd9ce17c89
mengkailiu <dave_lmk@outlook.com>
2023-04-16 11:13:47 +0800

    33. 搜索旋转排序数组 --go

---
+++ golang/33_search_in_rotated_sorted_array.go
@@ -0,0 +1,35 @@
+package main
+
+func search(nums []int, target int) int {
+	n := len(nums)
+	if n <= 0 {
+		return -1
+	}
+	if n == 1 && nums[0] != target {
+		return -1
+	}
+	if n == 1 && nums[0] == target {
+		return 0
+	}
+	l, r := 0, n-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[0] <= nums[mid] {
+			if nums[0] <= target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[n-1] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	return -1
+}
