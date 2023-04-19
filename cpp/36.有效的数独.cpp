3433c9299aa5d0fe7e6529210295b837a2fcc119
mengkailiu <dave_lmk@outlook.com>
2023-04-19 23:23:38 +0800

    36. 有效数独

--- cpp/36.有效的数独.cpp
+++ cpp/36.有效的数独.cpp
@@ -4,40 +4,15 @@
  * [36] 有效的数独
  */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 // @lc code=start
 class Solution {
    public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // unordered_set<char> row;
-        // unordered_set<char> column;
-        // unordered_set<char> cube;
-        // for (int i = 0; i < 9; i++) {
-        //     for (int j = 0; j < 9; j++) {
-        //         if (board[i][j] != '.' && row.find(board[i][j])!=row.end()) {
-        //             return false;
-        //         }
-        //         else {
-        //             row.insert(board[i][j]);
-        //         }
-        //         if (board[j][i] != '.' && column.find(board[j][i])!=column.end()) {
-        //             return false;
-        //         }
-        //         else {
-        //             column.insert(board[j][i]);
-        //         }
-        //         int row_index = 3 * (i / 3) + j / 3;
-        //         int column_index = 3 * (i % 3) + j % 3;
-        //         if (board[row_index][column_index] != '.' && cube.find(board[row_index][column_index])!=cube.end()) {
-        //             cout << row_index << "," << column_index << endl;
-        //             return false;
-        //         }
-        //         else {
-        //             cube.insert(board[row_index][column_index]);
-        //         }
-        //     }
-        // }
-        // return true;
-
         if (board.empty() || board[0].empty()) return false;
         int m = board.size(), n = board[0].size();
         vector<vector<bool>> rowFlag(m, vector<bool>(n, false));
