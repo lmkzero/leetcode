2818120696bafdd4e73015d3c96a0ccedf00f957
mengkailiu <dave_lmk@outlook.com>
2023-07-26 21:48:53 +0800

    876. 链表的中间结点

---
+++ golang/876_middle_of_the_linked_list.go
@@ -0,0 +1,13 @@
+package main
+
+func middleNode(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
