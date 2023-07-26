cfce682919075925289c8f063422b5cb1e83374b
mengkailiu <dave_lmk@outlook.com>
2023-07-26 22:13:07 +0800

    160. 相交链表

--- cpp/160.相交链表.cpp
+++ cpp/160.相交链表.cpp
@@ -13,6 +13,9 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+#include "define.h"
+
 class Solution {
    public:
     int getLength(ListNode *head) {
@@ -51,6 +54,26 @@ class Solution {
             headB = headB->next;
         }
         return nullptr;
+    }
+
+    ListNode *getIntersectionNodeByCombine(ListNode *headA, ListNode *headB) {
+        // p1 指向 A 链表头结点，p2 指向 B 链表头结点
+        ListNode *p1 = headA, *p2 = headB;
+        while (p1 != p2) {
+            // p1 走一步，如果走到 A 链表末尾，转到 B 链表
+            if (p1 == nullptr) {
+                p1 = headB;
+            } else {
+                p1 = p1->next;
+            }
+            // p2 走一步，如果走到 B 链表末尾，转到 A 链表
+            if (p2 == nullptr) {
+                p2 = headA;
+            } else {
+                p2 = p2->next;
+            }
+        }
+        return p1;  // 返回交点
     }
 };
 // @lc code=end
