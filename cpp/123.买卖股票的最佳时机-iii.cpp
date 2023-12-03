d422153c890c580d91fdb8ed75347aed12d44c6b
mengkailiu <dave_lmk@outlook.com>
2023-12-03 23:18:55 +0800

    123. 买卖股票的最佳时机 III

---
+++ cpp/123.买卖股票的最佳时机-iii.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=cpp
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int maxK = 2, n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(maxK + 1, vector<int>(2)));
+        for (int i = 0; i < n; i++) {
+            for (int k = maxK; k >= 1; k--) {
+                if (i - 1 == -1) {
+                    // 处理 base case
+                    dp[i][k][0] = 0;
+                    dp[i][k][1] = -prices[i];
+                    continue;
+                }
+                dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                dp[i][k][1] = max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+            }
+        }
+        return dp[n - 1][maxK][0];
+    }
+};
+// @lc code=end
---
+++ golang/123_best_time_to_buy_and_sell_stock_iii_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+func Test_maxProfitIII(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				prices: []int{3, 3, 5, 0, 0, 3, 1, 4},
+			},
+			want: 6,
+		},
+		{
+			name: "2",
+			args: args{
+				prices: []int{1, 2, 3, 4, 5},
+			},
+			want: 4,
+		},
+		{
+			name: "3",
+			args: args{
+				prices: []int{7, 6, 4, 3, 1},
+			},
+			want: 0,
+		},
+		{
+			name: "4",
+			args: args{
+				prices: []int{1},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfitIII(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfitIII() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
