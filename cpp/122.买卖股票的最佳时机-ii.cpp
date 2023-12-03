b0ce7d697e01a85cfa3c385116294f59c7603a3e
mengkailiu <dave_lmk@outlook.com>
2023-12-03 12:13:00 +0800

    122.买卖股票的最佳时机II

---
+++ cpp/122.买卖股票的最佳时机-ii.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=122 lang=cpp
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+};
+// @lc code=end
