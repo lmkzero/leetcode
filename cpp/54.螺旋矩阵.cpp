f46aa131c2d6d12f48d693d6d717bf1b2aa3454d
mengkailiu <dave_lmk@outlook.com>
2025-06-04 08:34:48 +0800

    54. 螺旋矩阵

---
+++ cpp/54.螺旋矩阵.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=cpp
+ *
+ * [54] 螺旋矩阵
+ */
+
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        int dirs[5] = {0, 1, 0, -1, 0};
+        int i = 0, j = 0, k = 0;
+        vector<int> ans;
+        bool vis[m][n];
+        memset(vis, false, sizeof(vis));
+        for (int h = m * n; h; --h) {
+            ans.push_back(matrix[i][j]);
+            vis[i][j] = true;
+            int x = i + dirs[k], y = j + dirs[k + 1];
+            if (x < 0 || x >= m || y < 0 || y >= n || vis[x][y]) {
+                k = (k + 1) % 4;
+            }
+            i += dirs[k];
+            j += dirs[k + 1];
+        }
+        return ans;
+    }
+};
+// @lc code=end
---
+++ golang/54_spiral_matrix.go
@@ -0,0 +1,22 @@
+package main
+
+func spiralOrder(matrix [][]int) []int {
+	m, n := len(matrix), len(matrix[0])
+	ans := make([]int, 0, m*n)
+	vis := make([][]bool, m)
+	for i := range vis {
+		vis[i] = make([]bool, n)
+	}
+	dirs := [5]int{0, 1, 0, -1, 0}
+	i, j, k := 0, 0, 0
+	for h := m * n; h > 0; h-- {
+		ans = append(ans, matrix[i][j])
+		vis[i][j] = true
+		x, y := i+dirs[k], j+dirs[k+1]
+		if x < 0 || x >= m || y < 0 || y >= n || vis[x][y] {
+			k = (k + 1) % 4
+		}
+		i, j = i+dirs[k], j+dirs[k+1]
+	}
+	return ans
+}
