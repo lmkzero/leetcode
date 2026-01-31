b865e18b8195d5d19340b552d92de4ff83c8b1f2
mengkailiu <dave_lmk@outlook.com>
2026-01-31 17:03:45 +0800

    236. 二叉树的最近公共祖先

---
+++ cpp/236.二叉树的最近公共祖先.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=cpp
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#include "define.h"
+
+class Solution {
+   public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) {
+            return root;
+        }
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) {
+            return root;
+        }
+        return left ? left : right;
+    }
+};
+// @lc code=end
