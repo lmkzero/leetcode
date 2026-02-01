f624173d7c527387c42018fe3766cb80c826e898
mengkailiu <dave_lmk@outlook.com>
2026-02-01 23:21:21 +0800

    update lc54

--- cpp/54.螺旋矩阵.cpp
+++ cpp/54.螺旋矩阵.cpp
@@ -13,20 +13,19 @@ class Solution {
    public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        int dirs[5] = {0, 1, 0, -1, 0};
+        int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         int i = 0, j = 0, k = 0;
         vector<int> ans;
-        bool vis[m][n];
-        memset(vis, false, sizeof(vis));
+        vector<vector<bool>> vis(m, vector<bool>(n));
         for (int h = m * n; h; --h) {
             ans.push_back(matrix[i][j]);
             vis[i][j] = true;
-            int x = i + dirs[k], y = j + dirs[k + 1];
+            int x = i + dirs[k][0], y = j + dirs[k][1];
             if (x < 0 || x >= m || y < 0 || y >= n || vis[x][y]) {
                 k = (k + 1) % 4;
             }
-            i += dirs[k];
-            j += dirs[k + 1];
+            i += dirs[k][0];
+            j += dirs[k][1];
         }
         return ans;
     }
--- golang/54_spiral_matrix.go
+++ golang/54_spiral_matrix.go
@@ -7,16 +7,16 @@ func spiralOrder(matrix [][]int) []int {
 	for i := range vis {
 		vis[i] = make([]bool, n)
 	}
-	dirs := [5]int{0, 1, 0, -1, 0}
+	dirs := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 	i, j, k := 0, 0, 0
 	for h := m * n; h > 0; h-- {
 		ans = append(ans, matrix[i][j])
 		vis[i][j] = true
-		x, y := i+dirs[k], j+dirs[k+1]
+		x, y := i+dirs[k][0], j+dirs[k][1]
 		if x < 0 || x >= m || y < 0 || y >= n || vis[x][y] {
 			k = (k + 1) % 4
 		}
-		i, j = i+dirs[k], j+dirs[k+1]
+		i, j = i+dirs[k][0], j+dirs[k][1]
 	}
 	return ans
 }
