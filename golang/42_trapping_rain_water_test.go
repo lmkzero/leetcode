9a2093ed044790bb92ec87688d5d1b3d182084df
mengkailiu <dave_lmk@outlook.com>
2026-02-04 22:54:06 +0800

    update lc42

--- golang/42_trapping_rain_water_test.go
+++ golang/42_trapping_rain_water_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_trap(t *testing.T) {
 	type args struct {
@@ -23,6 +25,32 @@ func Test_trap(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := trap(tt.args.height); got != tt.want {
 				t.Errorf("trap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trapDP(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1},
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trapDP(tt.args.height); got != tt.want {
+				t.Errorf("trapDP() = %v, want %v", got, tt.want)
 			}
 		})
 	}
