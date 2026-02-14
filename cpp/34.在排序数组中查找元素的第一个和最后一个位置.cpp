d3920ed8b9eef771964e31e97517a02cef578ea2
mengkailiu <dave_lmk@outlook.com>
2026-02-14 09:04:11 +0800

    update lc34

--- cpp/34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ cpp/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -4,7 +4,6 @@
  * [34] 在排序数组中查找元素的第一个和最后一个位置
  */
 
-#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -22,24 +21,30 @@ class Solution {
         int f = 0, l = nums.size() - 1;
         while (f <= l) {
             int m = (f + l) / 2;
-            if (nums[m] < target)
+            if (nums[m] < target) {
                 f = m + 1;
-            else
+            } else {
                 l = m - 1;
+            }
         }
-        if (f < nums.size() && nums[f] == target) return f;
+        if (f < nums.size() && nums[f] == target) {
+            return f;
+        }
         return -1;
     }
     int findLast(vector<int>& nums, int target) {
         int f = 0, l = nums.size() - 1;
         while (f <= l) {
             int m = (f + l) / 2;
-            if (nums[m] <= target)
+            if (nums[m] <= target) {
                 f = m + 1;
-            else
+            } else {
                 l = m - 1;
+            }
         }
-        if (l >= 0 && nums[l] == target) return l;
+        if (l >= 0 && nums[l] == target) {
+            return l;
+        }
         return -1;
     }
 };
