d2c7a55fc6ad6e86ba23d0828a2199352b82b448
mengkailiu <dave_lmk@outlook.com>
2026-02-20 22:27:59 +0800

    update lc122

--- golang/122_best_time_to_buy_and_sell_stock_ii.go
+++ golang/122_best_time_to_buy_and_sell_stock_ii.go
@@ -14,3 +14,15 @@ func maxProfitII(prices []int) int {
 	}
 	return dp[n-1][0]
 }
+
+// 从第二天开始，如果当天股价大于前一天股价，则在前一天买入，当天卖出，即可获得利润。如果当天股价小于前一天股价，则不买入，不卖出。
+// 也即是说，所有上涨交易日都做买卖，所有下跌交易日都不做买卖，最终获得的利润是最大的。
+func maxProfitIIGreedy(prices []int) (ans int) {
+	for i, v := range prices[1:] {
+		t := v - prices[i]
+		if t > 0 {
+			ans += t
+		}
+	}
+	return
+}
--- golang/122_best_time_to_buy_and_sell_stock_ii_test.go
+++ golang/122_best_time_to_buy_and_sell_stock_ii_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_maxProfitII(t *testing.T) {
 	type args struct {
@@ -37,6 +39,46 @@ func Test_maxProfitII(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := maxProfitII(tt.args.prices); got != tt.want {
 				t.Errorf("maxProfitII() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxProfitIIGreedy(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantAns int
+	}{
+		{
+			name: "1",
+			args: args{
+				prices: []int{7, 1, 5, 3, 6, 4},
+			},
+			wantAns: 7,
+		},
+		{
+			name: "2",
+			args: args{
+				prices: []int{1, 2, 3, 4, 5},
+			},
+			wantAns: 4,
+		},
+		{
+			name: "3",
+			args: args{
+				prices: []int{7, 6, 4, 3, 1},
+			},
+			wantAns: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotAns := maxProfitIIGreedy(tt.args.prices); gotAns != tt.wantAns {
+				t.Errorf("maxProfitIIGreedy() = %v, want %v", gotAns, tt.wantAns)
 			}
 		})
 	}
