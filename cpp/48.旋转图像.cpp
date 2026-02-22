2600688701022943c31994e02f8b90d701f5bced
mengkailiu <dave_lmk@outlook.com>
2026-02-22 23:33:59 +0800

    update lc48

--- cpp/48.旋转图像.cpp
+++ cpp/48.旋转图像.cpp
@@ -12,22 +12,17 @@ using namespace std;
 class Solution {
    public:
     void rotate(vector<vector<int>>& matrix) {
-        // const int n = matrix.size();
-
-        // for (int i = 0; i < n; ++i)  // 沿着副对角线反转
-        //     for (int j = 0; j < n - i; ++j)
-        //         swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
-
-        // for (int i = 0; i < n / 2; ++i) // 沿着水平中线反转
-        //     for (int j = 0; j < n; ++j)
-        //         swap(matrix[i][j], matrix[n - 1 - i][j]);
-
+        // 上下翻转
         for (int i = 0; i < matrix.size(); i++) {
-            for (int j = i; j < matrix[0].size(); j++) swap(matrix[i][j], matrix[j][i]);
+            for (int j = i; j < matrix[0].size(); j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
         }
-
+        // 主对角线翻转
         for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0, k = matrix[0].size() - 1; j < k; j++, k--) swap(matrix[i][j], matrix[i][k]);
+            for (int j = 0, k = matrix[0].size() - 1; j < k; j++, k--) {
+                swap(matrix[i][j], matrix[i][k]);
+            }
         }
     }
 };
