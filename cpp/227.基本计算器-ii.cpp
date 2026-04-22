4495c4bbef184b929e4bd431213c20719b8b2b39
mengkailiu <dave_lmk@outlook.com>
2026-04-22 23:50:11 +0800

    227. 基本计算器 II

---
+++ cpp/227.基本计算器-ii.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=227 lang=cpp
+ *
+ * [227] 基本计算器 II
+ */
+
+#include <stack>
+#include <string>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    int calculate(string s) {
+        int v = 0, n = s.size();
+        char sign = '+';
+        stack<int> stk;
+        for (int i = 0; i < n; ++i) {
+            char c = s[i];
+            if (isdigit(c)) v = v * 10 + (c - '0');
+            if (i == n - 1 || c == '+' || c == '-' || c == '*' || c == '/') {
+                if (sign == '+')
+                    stk.push(v);
+                else if (sign == '-')
+                    stk.push(-v);
+                else if (sign == '*') {
+                    int t = stk.top();
+                    stk.pop();
+                    stk.push(t * v);
+                } else {
+                    int t = stk.top();
+                    stk.pop();
+                    stk.push(t / v);
+                }
+                sign = c;
+                v = 0;
+            }
+        }
+        int ans = 0;
+        while (!stk.empty()) {
+            ans += stk.top();
+            stk.pop();
+        }
+        return ans;
+    }
+};
+// @lc code=end
